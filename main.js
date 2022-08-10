0adf9cf0dc4a297cba3ec423d58978b2ab231a93
a7mad1112 <ahmalawneh79@gmail.com>
2022-08-10 14:35:34 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+
+// Variables
+let theInput = document.querySelector(".get-repos input");
+let getButton = document.querySelector(".get-button");
+let reposData = document.querySelector(".show-data");
+getButton.onclick = () => {
+    getRepos();
+};
+function getRepos() {
+    if(theInput.value == ""){
+        reposData.innerHTML = "<span>Please Write Github Username.</span>";
+    } else {
+        fetch(`https://api.github.com/users/${theInput.value}/repos`)
+        .then(response => response.json())
+        .then(repos => {
+            reposData.innerHTML = "";
+            repos.forEach(repo => {
+                let mainDiv = document.createElement("div");
+                let repoName = document.createTextNode(repo.name);
+                mainDiv.className = "repo-box";
+                mainDiv.appendChild(repoName);
+                let theUrl = document.createElement("a");
+                let theUrlText = document.createTextNode("Visit");
+                theUrl.appendChild(theUrlText);
+                theUrl.href = `https://github.com/${theInput.value}/${repo.name}`;
+                theUrl.setAttribute("target", "_blank");
+                mainDiv.appendChild(theUrl);
+                let starsSpan = document.createElement("span");
+                let starsText = document.createTextNode(`Starts ${repo.stargazers_count}`);
+                starsSpan.appendChild(starsText);
+                mainDiv.appendChild(starsSpan);
+                reposData.appendChild(mainDiv);
+            });
+        })
+    }
+};
